2e9ebc63c9417dec844b0f1efe5a8818acc06cc3
santiagogracianod <santiago.gracianod@gmail.com>
2024-04-14 22:40:21 -0500

    Diseño inicial con menu de opciones y tabs

--- SistemaDeVotacion/Form1.Designer.cs
+++ SistemaDeVotacion/Form1.Designer.cs
@@ -28,47 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            btnVotar = new Button();
             pMenu = new Panel();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
             panel1 = new Panel();
             panel2 = new Panel();
             panel3 = new Panel();
+            panel4 = new Panel();
+            pMenu.SuspendLayout();
             SuspendLayout();
-            // 
-            // btnVotar
-            // 
-            btnVotar.Location = new Point(231, 313);
-            btnVotar.Name = "btnVotar";
-            btnVotar.Size = new Size(75, 23);
-            btnVotar.TabIndex = 0;
-            btnVotar.Text = "Votar";
-            btnVotar.UseVisualStyleBackColor = true;
             // 
             // pMenu
             // 
             pMenu.BackColor = Color.FromArgb(51, 62, 80);
+            pMenu.Controls.Add(button3);
+            pMenu.Controls.Add(button2);
+            pMenu.Controls.Add(button1);
             pMenu.Dock = DockStyle.Top;
             pMenu.Location = new Point(0, 0);
             pMenu.Name = "pMenu";
-            pMenu.Size = new Size(800, 100);
+            pMenu.Size = new Size(800, 45);
             pMenu.TabIndex = 1;
+            // 
+            // button3
+            // 
+            button3.Cursor = Cursors.Hand;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(253, 0);
+            button3.Name = "button3";
+            button3.Size = new Size(140, 40);
+            button3.TabIndex = 2;
+            button3.Text = "Administrador";
+            button3.UseVisualStyleBackColor = true;
+            button3.MouseEnter += btnMouseEnter;
+            button3.MouseLeave += btnMouseLeave;
+            // 
+            // button2
+            // 
+            button2.Cursor = Cursors.Hand;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(79, 0);
+            button2.Name = "button2";
+            button2.Size = new Size(140, 40);
+            button2.TabIndex = 1;
+            button2.Text = "Votantes";
+            button2.UseVisualStyleBackColor = true;
+            button2.MouseEnter += btnMouseEnter;
+            button2.MouseLeave += btnMouseLeave;
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Location = new Point(752, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(45, 45);
+            button1.TabIndex = 0;
+            button1.Text = "X";
+            button1.UseVisualStyleBackColor = true;
             // 
             // panel1
             // 
             panel1.BackColor = Color.FromArgb(51, 62, 80);
             panel1.Dock = DockStyle.Left;
-            panel1.Location = new Point(0, 100);
+            panel1.Location = new Point(0, 45);
             panel1.Name = "panel1";
-            panel1.Size = new Size(5, 350);
+            panel1.Size = new Size(5, 405);
             panel1.TabIndex = 2;
             // 
             // panel2
             // 
             panel2.BackColor = Color.FromArgb(51, 62, 80);
             panel2.Dock = DockStyle.Right;
-            panel2.Location = new Point(795, 100);
+            panel2.Location = new Point(795, 45);
             panel2.Name = "panel2";
-            panel2.Size = new Size(5, 350);
+            panel2.Size = new Size(5, 405);
             panel2.TabIndex = 3;
             // 
             // panel3
@@ -80,29 +120,42 @@
             panel3.Size = new Size(790, 8);
             panel3.TabIndex = 4;
             // 
+            // panel4
+            // 
+            panel4.BackColor = Color.LightGray;
+            panel4.BorderStyle = BorderStyle.Fixed3D;
+            panel4.Dock = DockStyle.Fill;
+            panel4.Location = new Point(5, 45);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(790, 397);
+            panel4.TabIndex = 5;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(panel4);
             Controls.Add(panel3);
             Controls.Add(panel2);
             Controls.Add(panel1);
             Controls.Add(pMenu);
-            Controls.Add(btnVotar);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
+            pMenu.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
-
-        private Button btnVotar;
         private Panel pMenu;
         private Panel panel1;
         private Panel panel2;
         private Panel panel3;
+        private Button button1;
+        private Panel panel4;
+        private Button button2;
+        private Button button3;
     }
 }
--- SistemaDeVotacion/Form1.cs
+++ SistemaDeVotacion/Form1.cs
@@ -6,10 +6,26 @@ namespace SistemaDeVotacion
         {
             InitializeComponent();
         }
+        
+        Panel p = new Panel();
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnMouseEnter(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            pMenu.Controls.Add(p);
+            p.BackColor = Color.FromArgb(90, 210, 2);
+            p.Size = new Size(140, 5);
+            p.Location = new Point(btn.Location.X, btn.Location.Y + 40);
+        }
+
+        private void btnMouseLeave(object sender, EventArgs e)
+        {
+            pMenu.Controls.Remove(p);
         }
     }
 }
